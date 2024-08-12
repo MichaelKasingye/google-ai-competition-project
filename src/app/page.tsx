5f3a444a4c00a4c1372f8e65a7096f675a68600d
MICHAEL KASINGYE <michaelkasingye@gmail.com>
2024-08-12 09:08:32 +0300

    edit home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,7 +94,7 @@ export default function Home() {
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none"></span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Huhumuza AI understands and shares your feelings, offering genuine
+            Huhumuza AI understands and shares your feelings, offering
             empathy.
           </p>
         </div>
