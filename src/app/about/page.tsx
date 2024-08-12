fc143e36254aae97e06e5dae1a5f0bacae77b47f
MICHAEL KASINGYE <michaelkasingye@gmail.com>
2024-08-12 09:31:01 +0300

    Edit about the page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -39,7 +39,9 @@ export default function About() {
       <div className="w-full max-w-4xl px-4 py-8 shadow-md rounded-lg">
         <h2 className="text-2xl font-semibold mb-4">AI Mission</h2>
         <p className="text-lg mb-4">
-          Huhumuza AI is a word in of the Ugandan tribes meaning to calm down. Huhumuza AI purpose to address the growing mental health crisis affecting millions of people across the United States and the world. Mental health issues, ranging from anxiety to depression, affect over 50 million Americans each year, with anxiety disorders being the most prevalent, impacting nearly 19.1% of adults. Depression affects 8.3% of adults and continues to be a significant challenge, especially as the impact of the COVID-19 pandemic exacerbates mental health conditions across all age groups.
+        Huhumuza is a word from one of the Ugandan tribes, meaning "to calm down." Huhumuza AI is dedicated to addressing the escalating mental health crisis affecting millions globally, including in the United States. In 2019, 970 million people worldwide were living with a mental disorder, with anxiety and depression being the most common. In the U.S., over 50 million people face mental health challenges each year, with anxiety disorders affecting nearly 19.1% of adults and depression impacting 8.3%.
+
+        The COVID-19 pandemic further intensified this crisis, with an 18% increase in depressive disorders and a 15% rise in anxiety disorders globally in 2020 compared to 2019. By 2021, 13.9% of the world’s population experienced mental disorders, with 71% of the global anxiety disorder burden potentially avoidable if optimal treatment was accessible to all. Mental health conditions can disrupt all areas of life, including relationships, education, and work, and they accounted for 17.2% of the total years lived with disability worldwide in 2021.
         </p>
         <p className="text-lg mb-4">
           Recognizing the need for accessible, empathetic, and effective mental health support, Huhumuza AI is designed to be your friendly, supportive, and empathetic companion. Whether you’re facing day-to-day stress or more serious mental health challenges, Huhumuza AI is here to help. The AI offers a safe and confidential space to talk, providing support and encouragement whenever you need it.
@@ -49,25 +51,25 @@ export default function About() {
           <div className="p-4  rounded-lg shadow-md">
             <h3 className="text-xl font-semibold mb-2">Empathy and Understanding</h3>
             <p className="text-md">
-              Huhumuza AI isn’t just an AI; it’s a friend who understands and shares your feelings. Whether you're dealing with anxiety, depression, or situational stress, Huhumuza is there to listen and provide comfort.
+              Huhumuza AI isn’t just an AI; it’s a friend who is designed to understandsand your feelings. Whether you're dealing with anxiety, depression, or situational stress, Huhumuza is there to listen and provide comfort.
             </p>
           </div>
           <div className="p-4 rounded-lg shadow-md">
             <h3 className="text-xl font-semibold mb-2">Accessible Anytime, Anywhere</h3>
             <p className="text-md">
-              In a world where 55% of adults with mental illness don’t receive the care they need due to cost or other barriers, Huhumuza AI breaks down these obstacles by being available anytime you need to talk, offering support without the need for an appointment or insurance.
+              In a world where more than 50% of adults with mental illness don’t receive the care they need due to cost or other barriers, Huhumuza AI breaks down these obstacles by being available anytime you need to talk.
             </p>
           </div>
           <div className="p-4 rounded-lg shadow-md">
             <h3 className="text-xl font-semibold mb-2">Support for All Ages</h3>
             <p className="text-md">
-              Mental health challenges can begin as early as age 14, with 50% of lifelong mental illnesses starting by this age. Huhumuza AI provides tailored support for users of all ages, from adolescents to adults, ensuring that no one faces their struggles alone.
+              Mental health challenges can begin as early as age 14, with more than 45% of lifelong mental illnesses starting by this age. Huhumuza AI provides support for users of all ages, from adolescents to adults, ensuring that no one faces their struggles alone.
             </p>
           </div>
           <div className="p-4 rounded-lg shadow-md">
             <h3 className="text-xl font-semibold mb-2">Combatting Isolation</h3>
             <p className="text-md">
-              During the COVID-19 pandemic, nearly 45.2% of adults report that the pandemic harms their mental health. Huhumuza AI provides a supportive connection, helping to alleviate feelings of isolation and loneliness.
+              During the COVID-19 pandemic, more than 40% of adults report that the pandemic harms their mental health. Huhumuza AI provides a supportive connection, helping to alleviate feelings of isolation and loneliness.
             </p>
           </div>
         </div>
@@ -78,8 +80,10 @@ export default function About() {
         {loading?"loading...": "Get started"}  
         </button>
         <div className="m-2">
+        (Resarch source: <a href="https://www.who.int/health-topics/mental-health#tab=tab_2" target="_blank" className="text-violet-500 hover:underline">WHO</a>),
+        (Resarch source: <a href="https://www.healthdata.org/research-analysis/health-risks-issues/mental-health" target="_blank" className="text-violet-500 hover:underline">Healthdata</a>),
         (Resarch source: <a href="https://www.usatoday.com/money/blueprint/health-insurance/mental-health-statistics/" target="_blank" className="text-violet-500 hover:underline">USA Today</a>), 
-        (Resarch source: <a href="https://www.usa.edu/blog/mental-health-statistics/" target="_blank" className="text-violet-500 hover:underline">University of St. Augustine for Health Sciences</a>).
+        (Resarch source: <a href="https://www.usa.edu/blog/mental-health-statistics/" target="_blank" className="text-violet-500 hover:underline">University of St. Augustine for Health Sciences</a>),
         </div>
       </div>
     </main>
